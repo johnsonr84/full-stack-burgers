f7327120e0c537755984c6ec3fa3eda87aa228f5
Your Name <robertcjohnson1984@gmail.com>
2021-08-19 21:00:51 -0600

    update get, post and put route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,32 +2,40 @@ const express = require('express');
 const burger = require('../models/burgers.js');
 const router = express.Router();
 
-// Routes
+// Get Route
 router.get('/', (req, res) => {
-    burger.select((data) => {
-        const hbsObject = {
-            burgers: data,
-        };
-        console.log(hbsObject);
-        res.render('index', hbsObject);
+    burger.select(data => {
+      const hbsObject = {
+        burgers: data
+      };
+      res.render("index", hbsObject);
     });
-});
-
+  });
+// Post Route
 router.post('/api/burgers', (req, res) => {
-    burger.insert([req.body.name], (result) => {
-        res.json(result);
-        res.status(200).end();
-    });
-});
-
+    burger.insert(
+      ["burger_name", "devoured"],
+      [req.body.burger_name, req.body.devoured],
+      (result) => {
+        res.json({ id: result.insertId });
+      }
+    );
+  });
+// Put Route
 router.put('/api/burgers/:id', (req, res) => {
-    burger.update(id, (result) => {
-            if (result.changedRows === 0) {
-                return res.status(404).end();
-            }
-            res.status(200).end();
-    })
-})
+    let condition = "id = " + req.params.id;
+    
+    burger.update(
+      { devoured: req.body.devoured },
+      condition,
+      (result) => {
+        if (result.changedRows === 0) {
+          return res.status(404).end();
+        } else {
+          res.status(200).end();
+        }
+      });
+  });
 
 // Export routes for server.js to use.
 module.exports = router;
