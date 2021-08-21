93eea7adde63b59c3771f3dc51a6a96765f8857d
Your Name <robertcjohnson1984@gmail.com>
2021-08-20 18:49:17 -0600

    add connection for controllers

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+
+const connection = require("./config/connection");
+const routes = require("./controllers/burgers_controller");
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 //Serve static content for the app from the 'Public' directory in the app directory
 app.use(express.static("public"));
 
-// Set Handlebars.
-const exphbs = require('express-handlebars');
+
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
@@ -17,8 +20,6 @@ app.use(express.json());
 //Handglebars setup
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-// Import routes and give the server access to them.
-const routes = require("./controllers/burgers_controller");
 
 //Use routes
 app.use(routes);
