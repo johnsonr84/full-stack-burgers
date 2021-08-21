33e7801a5d2bbc1ea7c0e4e88f9a057ba50395e7
Your Name <robertcjohnson1984@gmail.com>
2021-08-20 18:56:48 -0600

    add update one

--- config/orm.js
+++ config/orm.js
@@ -1,28 +1,62 @@
 // Import MySQL connection.
 const connection = require('./connection.js');
 
-const orm = {
-    selectAll(table, cb) {
-        connection.query(`SELECT * FROM ${table}`, (err, result) => {
-            if (err) throw err;
-            cb(result);
-        })
-    },
 
-    insertOne(reqBody, name, cb) {
-        connection.query(`INSERT INTO ?? SET name = ?`, [reqBody, name], (err, result) => {
-            if (err) throw err;
-            cb(result);
-        })
-    },
-
-    updateOne(id, cb) {
-        connection.query('UPDATE burgers SET eaten = 1 WHERE id = ?', [id], (err, result) => {
-            if (err) throw err;
-            cb(result)
-        })
+function createQmarks(num) {
+    let arr = [];
+    for (let i = 0; i < num; i++) {
+      arr.push("?");
     }
-}
+    return arr.toString(); 
+  }
+  
+  function translateSql(obj) {
+    let arr = [];
+    for (let key in obj) {
+      value = obj[key];
+      if (Object.hasOwnProperty.call(obj, key)) {
+        // if string with spaces, add quotations 
+        if (typeof value === "string" && value.indexOf(" " >= 0)) {
+          value = "'" + value + "'";
+        }
+        arr.push(key + "=" + value)
+      }
+    }
+    return arr.toString();
+  }
+
+const orm = {
+    //select all from the table
+    selectAll: (table, cb) => {
+      let queryStr = "SELECT * FROM " + table + ";";
+  
+      connection.query(queryStr, (err, result) => {
+        if (err) throw err; 
+        cb(result)
+      })
+    },
+  
+    //insert method
+    insertOne: (table, cols, vals, cb) => {
+      let queryStr = "INSERT INTO " + table + " (" + cols.toString() + ") " + "VALUES (" + createQmarks(vals.length) + ") ";
+      
+      connection.query(queryStr, vals, (err, result) => {
+        if (err) throw err;
+        cb(result)
+      });
+    }, 
+  
+    //update method 
+    updateOne: (table, objColVals, condition, cb) => {
+      let queryStr = "UPDATE " + table + " SET " + translateSql(objColVals) + " WHERE " + condition; 
+  
+      connection.query(queryStr, (err, result) => {
+        if (err) throw err;
+        cb(result)
+      })
+    },
+  
+  }
 
 // Export the orm object for the model (burgers.js).
 module.exports = orm;
