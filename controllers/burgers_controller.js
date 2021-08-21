4c95414e239c22bfaaca14ebaaeaf823c70cce3f
Your Name <robertcjohnson1984@gmail.com>
2021-08-20 18:58:38 -0600

    update require burger

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const burger = require('../models/burgers.js');
+const burger = require('../models/burger.js');
 const router = express.Router();
 
 // Get Route
